5908e1a81d23504026f67af7bb3d446d59a236a5
OscarMacoto <oscar92macos@hotmail.com>
2026-01-05 08:48:55 -0600

    Updated

--- frontend/src/pages/Empleados.jsx
+++ frontend/src/pages/Empleados.jsx
@@ -759,7 +759,7 @@ const importarExcel = async (file) => {
         />
         <button
           onClick={limpiarFiltros}
-          className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-400"
+          className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
         >
           Limpiar
         </button>
